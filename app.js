2aef15787d0164797a806e2e7afc718baceb2f36
verter187 <verter187@inbox.ru>
2023-02-26 14:58:26 +0300

    Enums

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 "use strict";
-const skill = [1, "Dev"];
-// const skills: readonly string[] = ["Dev", "Devops"];
-// const skills: Array<string> = ["Dev", "Devops"];
-const skills = ["Dev", "Devops"];
-// skills.push("test");
-// skills[0] = "test";
+var StatusCode;
+(function (StatusCode) {
+    StatusCode["SUCCESS"] = "ssd";
+    StatusCode["IN_PROCESS"] = "p";
+    StatusCode["FAILED"] = "f";
+})(StatusCode || (StatusCode = {}));
+const res = {
+    message: "Платеж успешен",
+    statusCode: StatusCode.SUCCESS,
+};
+//1 - Успех
+//2 - в процессе
+//3 - отклонен
+if (res.statusCode === StatusCode.SUCCESS) {
+    //proccessing
+}
+function action(status) { }
+action(StatusCode.SUCCESS);
+const res2 = 1 /* Roles.ADMIN */;
+// enum Roles {
+//   ADMIN = 1,
+//   USER = compute(),
+// }
+// const test = (x: { ADMIN: number }) => {};
--- app.ts
+++ app.ts
@@ -1,8 +1,40 @@
-const skill: readonly [number, string] = [1, "Dev"];
+enum StatusCode {
+  SUCCESS = "ssd",
+  IN_PROCESS = "p",
+  FAILED = "f",
+}
 
-// const skills: readonly string[] = ["Dev", "Devops"];
-// const skills: Array<string> = ["Dev", "Devops"];
+const res = {
+  message: "Платеж успешен",
+  statusCode: StatusCode.SUCCESS,
+};
 
-const skills: ReadonlyArray<string> = ["Dev", "Devops"];
-// skills.push("test");
-// skills[0] = "test";
+//1 - Успех
+//2 - в процессе
+//3 - отклонен
+
+if (res.statusCode === StatusCode.SUCCESS) {
+  //proccessing
+}
+
+function action(status: StatusCode) {}
+
+action(StatusCode.SUCCESS);
+// action(1);
+
+// function compute() {
+//   return 3;
+// }
+
+const enum Roles {
+  ADMIN = 1,
+  USER = 2,
+}
+
+const res2 = Roles.ADMIN;
+// enum Roles {
+//   ADMIN = 1,
+//   USER = compute(),
+// }
+
+// const test = (x: { ADMIN: number }) => {};
