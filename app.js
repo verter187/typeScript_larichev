9c0721c904fcd48aaf479a3dcbc9ddd1fafeb912
verter187 <verter187@inbox.ru>
2023-02-26 18:49:40 +0300

    Type Aliases

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 "use strict";
-// enum RequestType {
-//   GET = "get",
-//   POST = "post",
-// }
 function fetchWithAuth(url, method) {
     console.log(method);
-    return -1;
+    return 1;
 }
-let method = "post";
+let method = "get";
 fetchWithAuth("test", method);
+// type UserWithRole = { user: User; role: Role };
+let user = {
+    name: "Stein",
+    age: 33,
+    id: 1,
+    skills: ["dev", "devOps"],
+};
--- app.ts
+++ app.ts
@@ -1,12 +1,26 @@
-// enum RequestType {
-//   GET = "get",
-//   POST = "post",
-// }
+type httpMethod = "post" | "get";
+type coolString = string;
 
-function fetchWithAuth(url: string, method: "post" | "get"): 1 | -1 {
+function fetchWithAuth(url: string, method: httpMethod): 1 | -1 {
   console.log(method);
-  return -1;
+  return 1;
 }
 
-let method: "post" = "post";
+let method: httpMethod = "get";
 fetchWithAuth("test", method);
+
+type User =
+  | { name: string; age: number; skills: string[] }
+  | { name: string; age: number };
+
+type Role = { id: number; name: string };
+
+type UserWithRole = User & Role;
+// type UserWithRole = { user: User; role: Role };
+
+let user: UserWithRole = {
+  name: "Stein",
+  age: 33,
+  id: 1,
+  skills: ["dev", "devOps"],
+};
