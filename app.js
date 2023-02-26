c848acab116250a1f5ea93a80b2897be7712394f
verter187 <verter187@inbox.ru>
2023-02-26 18:27:12 +0300

    Literal Types

--- app.js
+++ app.js
@@ -1,41 +1,11 @@
 "use strict";
-function logId(id) {
-    if (typeof id === "string") {
-        id.toLowerCase();
-    }
-    else if (typeof id === "number") {
-        console.log(id);
-    }
-    else {
-        console.log(id);
-    }
-    console.log(id);
+// enum RequestType {
+//   GET = "get",
+//   POST = "post",
+// }
+function fetchWithAuth(url, method) {
+    console.log(method);
+    return -1;
 }
-// logId(1);
-// logId("sdfsdf");
-// logId(false);
-function logError(err) {
-    if (Array.isArray(err)) {
-        console.log(err);
-    }
-    else {
-        console.log(err);
-    }
-}
-function logObject(obj) {
-    if ("a" in obj) {
-        console.log(obj.a);
-    }
-    else {
-        console.log(obj.b);
-    }
-}
-function logMultipleIds(a, b) {
-    if (a === b) {
-        console.log(a, b);
-    }
-    else {
-        console.log(a, b);
-    }
-}
-const a = 1;
+let method = "post";
+fetchWithAuth("test", method);
--- app.ts
+++ app.ts
@@ -1,39 +1,12 @@
-function logId(id: string | number | boolean): void {
-  if (typeof id === "string") {
-    id.toLowerCase();
-  } else if (typeof id === "number") {
-    console.log(id);
-  } else {
-    console.log(id);
-  }
-  console.log(id);
+// enum RequestType {
+//   GET = "get",
+//   POST = "post",
+// }
+
+function fetchWithAuth(url: string, method: "post" | "get"): 1 | -1 {
+  console.log(method);
+  return -1;
 }
 
-// logId(1);
-// logId("sdfsdf");
-// logId(false);
-function logError(err: string | string[]): void {
-  if (Array.isArray(err)) {
-    console.log(err);
-  } else {
-    console.log(err);
-  }
-}
-
-function logObject(obj: { a: number } | { b: number }): void {
-  if ("a" in obj) {
-    console.log(obj.a);
-  } else {
-    console.log(obj.b);
-  }
-}
-
-function logMultipleIds(a: string | number, b: string | boolean): void {
-  if (a === b) {
-    console.log(a, b);
-  } else {
-    console.log(a, b);
-  }
-}
-
-const a: 1 = 1;
+let method: "post" = "post";
+fetchWithAuth("test", method);
