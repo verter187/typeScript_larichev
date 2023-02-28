0cc5a674f9aa25d6bf33d24b9dcd655789660379
verter187 <verter187@inbox.ru>
2023-02-28 18:32:49 +0300

    Optional

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 "use strict";
 // interface User {
-//   name: string;
+//   login: string;
+//   password?: string;
 // }
+// type User = {
+//   login: string;
+//   password?: string;
+// };
+const user = {
+    login: "a@a.ru",
+    password: undefined,
+};
+//function multiply(first: number, second?: number): number {
+function multiply(first, second = 5) {
+    if (!second) {
+        return first * first;
+    }
+    return first * second;
+}
+console.log(multiply(5));
+function testPass(user) {
+    var _a;
+    const t = (_a = user.password) === null || _a === void 0 ? void 0 : _a.type;
+}
--- app.ts
+++ app.ts
@@ -1,26 +1,42 @@
 // interface User {
-//   name: string;
+//   login: string;
+//   password?: string;
 // }
 
-// interface User {
-//   age: number;
-// }
+interface User {
+  login: string;
+  password: string | undefined;
+}
 
 // type User = {
-//   name: string;
+//   login: string;
+//   password?: string;
 // };
 
-// type User = {
-//   age: number;
-// };
+const user: User = {
+  login: "a@a.ru",
+  password: undefined,
+};
 
-// const user: User = {
-//   name: "John",
-//   age: 36,
-// };
+//function multiply(first: number, second?: number): number {
+function multiply(first: number, second: number = 5): number {
+  if (!second) {
+    return first * first;
+  }
+  return first * second;
+}
 
-type ID = string | number;
+console.log(multiply(5));
 
-interface IDI {
-  ID: string | number;
+interface UserPro {
+  login: string;
+  password?: { type: "primary | secondary" };
+}
+
+function testPass(user: UserPro) {
+  const t = user.password?.type;
+}
+
+function test(param?: string) {
+  const t = param ?? multiply(5);
 }
