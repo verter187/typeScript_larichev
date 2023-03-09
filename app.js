b5efa53de30ddec0b865c2698c499745ea3ac04d
verter187 <verter187@inbox.ru>
2023-03-09 21:09:59 +0300

    Abstract classes

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 "use strict";
-class UserBuilder {
-    setName(name) {
-        this.name = name;
-        return this;
-    }
-    isAdmin() {
-        return this instanceof AdminBuilder;
+class Controller {
+    handleWithLogs(req) {
+        console.log("Start");
+        this.handle(req);
+        console.log("End");
     }
 }
-class AdminBuilder extends UserBuilder {
+// new Controller(); //Так создать экземпляр абстрактного класса нельзя, можно создать экземляр только наследуюемого класса.
+class UserController extends Controller {
+    //Абстрактные классы обязательно должны быть в субклассе!
+    handle(req) {
+        console.log(req);
+    }
 }
-const res = new UserBuilder().setName("Vasya");
-const res2 = new AdminBuilder().setName("Vasya");
-let user = new UserBuilder();
-if (user.isAdmin()) {
-    console.log(user);
-}
-else {
-    console.log(user);
-}
+new UserController(); //Можно создать экземляр класса, унаследованного от абстрактного класса.
+//Абстрактный класс в отличие от интерфейса может в себе реализовывать какой-либо функционал и при наследовании мы получаем
+//весь этот функционал (чего нельзя сделать с интерфейсами). К тому же, мы можем обращаться внутри абстрактного класса из
+//необстрактных методов к абстрактным методам.
+//Абстрактные методы могут быть тольков абстрактном классе.
+const c = new UserController();
+c.handleWithLogs("Request");
--- app.ts
+++ app.ts
@@ -1,25 +1,29 @@
-class UserBuilder {
-  name: string;
-  setName(name: string): this {
-    this.name = name;
-    return this;
-  }
+abstract class Controller {
+  abstract handle(req: any): void;
 
-  isAdmin(): this is AdminBuilder {
-    return this instanceof AdminBuilder;
+  handleWithLogs(req: any) {
+    console.log("Start");
+    this.handle(req);
+    console.log("End");
   }
 }
 
-class AdminBuilder extends UserBuilder {
-  roles: string[];
-}
-const res = new UserBuilder().setName("Vasya");
-const res2 = new AdminBuilder().setName("Vasya");
+// new Controller(); //Так создать экземпляр абстрактного класса нельзя, можно создать экземляр только наследуюемого класса.
 
-let user: UserBuilder | AdminBuilder = new UserBuilder();
-
-if (user.isAdmin()) {
-  console.log(user);
-} else {
-  console.log(user);
+class UserController extends Controller {
+  //Абстрактные классы обязательно должны быть в субклассе!
+  handle(req: any): void {
+    console.log(req);
+  }
 }
+
+new UserController(); //Можно создать экземляр класса, унаследованного от абстрактного класса.
+
+//Абстрактный класс в отличие от интерфейса может в себе реализовывать какой-либо функционал и при наследовании мы получаем
+//весь этот функционал (чего нельзя сделать с интерфейсами). К тому же, мы можем обращаться внутри абстрактного класса из
+//необстрактных методов к абстрактным методам.
+
+//Абстрактные методы могут быть тольков абстрактном классе.
+
+const c = new UserController();
+c.handleWithLogs("Request");
