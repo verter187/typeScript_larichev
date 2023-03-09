ce3045837e6df55fe0b5286b8c055f996938acd3
verter187 <verter187@inbox.ru>
2023-03-09 19:55:24 +0300

    Work with 'this'

--- app.ts
+++ app.ts
@@ -1,30 +1,30 @@
-class UserService {
-  // static name: string = "UserService";//Name конфликтует со встроенным function.name
-  static db: any;
-  static getUser(id: number) {
-    // return this.db.findById(id);
-    return UserService.db.findById(id); //Так также можно обращаться к статичным методам и изнутри класса.
-  }
-  static async getUserAsync(id: number) {
-    // return this.db.findById(id);
-    return UserService.db.findById(id); //Так также можно обращаться к статичным методам и изнутри класса.
+class Payment {
+  private date: Date = new Date();
+
+  getDate(this: Payment) {
+    return this.date;
   }
 
-  create() {
-    UserService.db;
-  }
+  getDateArrow = () => this.date;
+}
 
-  //Возможность инициализации для статичных свойств класса(без создания экземпляра класса)
-  static {
-    // await new Promise(); //Асинхронные методы не могут работать внутри блока static.
-    UserService.db = "sdf";
+const p = new Payment();
+
+const user = {
+  id: 1,
+  paymentDate: p.getDate.bind(p),
+  paymentDateArrow: p.getDateArrow,
+};
+
+console.log(p.getDate());
+console.log(user.paymentDate());
+console.log(user.paymentDateArrow());
+
+class PaymentPersistent extends Payment {
+  save() {
+    // return super.getDateArrow(); //стрелочные методы не содержатся в прототипах и не доступны через super, будет вызвана ошибка.
+    return this.getDateArrow(); // Но через this данные будут получены.
   }
 }
 
-UserService.db; //
-UserService.getUser(1); // Статичные методы можно выполнять без создания экземпляра класса.
-
-const inst = new UserService();
-inst.create(); //При создании экземпляра класса теряем доступ к его статичным методам.
-
-// В js не нужно делать классы асинхронными, можно использовать отдельные асинхронные методы класса.
+console.log(new PaymentPersistent().save());
