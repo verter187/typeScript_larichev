85d442493382b8dfc317a77a35263054111659c1
verter187 <verter187@inbox.ru>
2023-03-11 14:22:02 +0300

    Writing a Function with a Generic

--- build/app.js
+++ build/app.js
@@ -1,22 +1,11 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-const num = [1, 2, 3];
-function test() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const a = new Promise((resolve, reject) => {
-            resolve(1);
-        });
-    });
+function logMiddleware(data) {
+    console.log(data);
+    return data;
 }
-const check = {
-    drive: true,
-    kpp: false,
-};
+const res = logMiddleware(10);
+function getSplitedHalf(data) {
+    const l = data.length / 2;
+    return data.splice(0, l);
+}
+console.log(getSplitedHalf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
-const num: Array<number> = [1, 2, 3];
-
-async function test() {
-  const a = new Promise<number>((resolve, reject) => {
-    resolve(1);
-  });
+function logMiddleware<T>(data: T): T {
+  console.log(data);
+  return data;
 }
 
-const check: Record<string, boolean> = {
-  drive: true,
-  kpp: false,
-};
+const res = logMiddleware<number>(10);
+
+function getSplitedHalf<T>(data: Array<T>): Array<T> {
+  const l = data.length / 2;
+  return data.splice(0, l);
+}
+
+console.log(getSplitedHalf<Number>([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
