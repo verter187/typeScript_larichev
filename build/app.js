9483df0d6aaa62f1dcef49a07b72cba8bc285222
verter187 <verter187@inbox.ru>
2023-03-13 21:28:57 +0300

    Typeof

--- build/app.js
+++ build/app.js
@@ -1,53 +1,23 @@
 "use strict";
-/*
-Необходимо написать функцию группировки, которая принимает массив объектов
-и его ключ, производит группировку по указанному ключу и возращает
-сгруппированный объект.
-Пример:
-``` js
-[
-    { group: 1, name: 'a' },
-    { group: 1, name: 'b' },
-    { group: 2, name: 'c' },
-];
-```
-
-При группировке по 'group' ---->
-
-``` js
-{
-    '1': [ { group: 1, name: 'a' }, { group: 1, name: 'b' } ],
-    '2': [ { group: 2, name: 'c' } ]
+let strOrNum = 5;
+if (Math.random() > 0.5) {
+    strOrNum = 5;
 }
-```
-*/
-const data = [
-    { group: 1, name: "a" },
-    { group: 1, name: "b" },
-    { group: 2, name: "c" },
-];
-function group(array, key) {
-    return array.reduce((map, item) => {
-        const itemKey = item[key];
-        let curEl = map[itemKey];
-        if (Array.isArray(curEl)) {
-            curEl.push(item);
-        }
-        else {
-            curEl = [item];
-        }
-        map[itemKey] = curEl;
-        return map;
-    }, {});
+else {
+    strOrNum = "str";
 }
-console.log(group(data, "group"));
-// function group<T, K extends keyof T>(data: T[], key: K) {
-//   return data
-//     .reduce(
-//       (acc: [], cur: T) =>
-//         acc.includes(cur[key]) ? acc : acc.push(cur[key]) && acc,
-//       []
-//     )
-//     .map((i) => ({ [i]: data.filter((obj) => obj[key] === i) }));
-// }
-// console.log(groupData(data, "group"));
+if (typeof strOrNum === "string") {
+    console.log(strOrNum);
+}
+else {
+    console.log(strOrNum);
+}
+let str2OrNum;
+const user = {
+    name: "Vasya",
+};
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 0] = "Up";
+    Direction[Direction["Down"] = 1] = "Down";
+})(Direction || (Direction = {}));
--- src/app.ts
+++ src/app.ts
@@ -1,65 +1,27 @@
-/*
-Необходимо написать функцию группировки, которая принимает массив объектов
-и его ключ, производит группировку по указанному ключу и возращает
-сгруппированный объект.
-Пример:
-``` js
-[
-	{ group: 1, name: 'a' },
-	{ group: 1, name: 'b' },
-	{ group: 2, name: 'c' },
-];
-```
-
-При группировке по 'group' ---->
-
-``` js
-{
-	'1': [ { group: 1, name: 'a' }, { group: 1, name: 'b' } ],
-	'2': [ { group: 2, name: 'c' } ]
-}
-```
-*/
-
-interface Data {
-  group: number;
-  name: string;
+let strOrNum: string | number = 5;
+if (Math.random() > 0.5) {
+  strOrNum = 5;
+} else {
+  strOrNum = "str";
 }
 
-const data: Data[] = [
-  { group: 1, name: "a" },
-  { group: 1, name: "b" },
-  { group: 2, name: "c" },
-];
-
-interface IGroup<T> {
-  [key: string]: T[];
+if (typeof strOrNum === "string") {
+  console.log(strOrNum);
+} else {
+  console.log(strOrNum);
 }
 
-type key = string | number | symbol;
+let str2OrNum: typeof strOrNum;
 
-function group<T extends Record<key, any>>(array: T[], key: keyof T) {
-  return array.reduce<IGroup<T>>((map: IGroup<T>, item) => {
-    const itemKey = item[key];
-    let curEl = map[itemKey];
-    if (Array.isArray(curEl)) {
-      curEl.push(item);
-    } else {
-      curEl = [item];
-    }
-    map[itemKey] = curEl;
-    return map;
-  }, {});
+const user = {
+  name: "Vasya",
+};
+
+type keyOfUser = keyof typeof user;
+
+enum Direction {
+  Up,
+  Down,
 }
-console.log(group<Data>(data, "group"));
-// function group<T, K extends keyof T>(data: T[], key: K) {
-//   return data
-//     .reduce(
-//       (acc: [], cur: T) =>
-//         acc.includes(cur[key]) ? acc : acc.push(cur[key]) && acc,
-//       []
-//     )
-//     .map((i) => ({ [i]: data.filter((obj) => obj[key] === i) }));
-// }
 
-// console.log(groupData(data, "group"));
+type d = keyof typeof Direction;
