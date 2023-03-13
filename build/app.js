cea7af45aeb3dce61459f7b080822128e05e61b5
verter187 <verter187@inbox.ru>
2023-03-13 21:12:07 +0300

    Writing a grouping function

--- build/app.js
+++ build/app.js
@@ -1,10 +1,53 @@
 "use strict";
-const key = "age";
-function getValue(obj, key) {
-    return obj[key];
+/*
+Необходимо написать функцию группировки, которая принимает массив объектов
+и его ключ, производит группировку по указанному ключу и возращает
+сгруппированный объект.
+Пример:
+``` js
+[
+    { group: 1, name: 'a' },
+    { group: 1, name: 'b' },
+    { group: 2, name: 'c' },
+];
+```
+
+При группировке по 'group' ---->
+
+``` js
+{
+    '1': [ { group: 1, name: 'a' }, { group: 1, name: 'b' } ],
+    '2': [ { group: 2, name: 'c' } ]
 }
-const user = {
-    name: "Vasya",
-    age: 30,
-};
-const userName = getValue(user, "name");
+```
+*/
+const data = [
+    { group: 1, name: "a" },
+    { group: 1, name: "b" },
+    { group: 2, name: "c" },
+];
+function group(array, key) {
+    return array.reduce((map, item) => {
+        const itemKey = item[key];
+        let curEl = map[itemKey];
+        if (Array.isArray(curEl)) {
+            curEl.push(item);
+        }
+        else {
+            curEl = [item];
+        }
+        map[itemKey] = curEl;
+        return map;
+    }, {});
+}
+console.log(group(data, "group"));
+// function group<T, K extends keyof T>(data: T[], key: K) {
+//   return data
+//     .reduce(
+//       (acc: [], cur: T) =>
+//         acc.includes(cur[key]) ? acc : acc.push(cur[key]) && acc,
+//       []
+//     )
+//     .map((i) => ({ [i]: data.filter((obj) => obj[key] === i) }));
+// }
+// console.log(groupData(data, "group"));
--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,65 @@
-interface IUser {
+/*
+Необходимо написать функцию группировки, которая принимает массив объектов
+и его ключ, производит группировку по указанному ключу и возращает
+сгруппированный объект.
+Пример:
+``` js
+[
+	{ group: 1, name: 'a' },
+	{ group: 1, name: 'b' },
+	{ group: 2, name: 'c' },
+];
+```
+
+При группировке по 'group' ---->
+
+``` js
+{
+	'1': [ { group: 1, name: 'a' }, { group: 1, name: 'b' } ],
+	'2': [ { group: 2, name: 'c' } ]
+}
+```
+*/
+
+interface Data {
+  group: number;
   name: string;
-  age: number;
 }
 
-type KeysOfUser = keyof IUser;
+const data: Data[] = [
+  { group: 1, name: "a" },
+  { group: 1, name: "b" },
+  { group: 2, name: "c" },
+];
 
-const key: KeysOfUser = "age";
-
-function getValue<T, K extends keyof T>(obj: T, key: K) {
-  return obj[key];
+interface IGroup<T> {
+  [key: string]: T[];
 }
 
-const user: IUser = {
-  name: "Vasya",
-  age: 30,
-};
+type key = string | number | symbol;
 
-const userName = getValue(user, "name");
+function group<T extends Record<key, any>>(array: T[], key: keyof T) {
+  return array.reduce<IGroup<T>>((map: IGroup<T>, item) => {
+    const itemKey = item[key];
+    let curEl = map[itemKey];
+    if (Array.isArray(curEl)) {
+      curEl.push(item);
+    } else {
+      curEl = [item];
+    }
+    map[itemKey] = curEl;
+    return map;
+  }, {});
+}
+console.log(group<Data>(data, "group"));
+// function group<T, K extends keyof T>(data: T[], key: K) {
+//   return data
+//     .reduce(
+//       (acc: [], cur: T) =>
+//         acc.includes(cur[key]) ? acc : acc.push(cur[key]) && acc,
+//       []
+//     )
+//     .map((i) => ({ [i]: data.filter((obj) => obj[key] === i) }));
+// }
+
+// console.log(groupData(data, "group"));
