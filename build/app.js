fd311613513fc44793b1e13cf254684405d1b13b
verter187 <verter187@inbox.ru>
2023-03-14 19:07:33 +0300

    Conditional Types

--- build/app.js
+++ build/app.js
@@ -1,11 +1,32 @@
 "use strict";
-const user = {
-    name: "Vasya",
-    roles: [],
-    permission: {
-        endDate: new Date(),
-    },
+const a1 = Math.random() > 0.5 ? 1 : 0;
+const suc = {
+    code: 200,
+    data: "done",
 };
-const nameUser = user["name"];
-const roleNames = "roles";
-const roles = ["admin", "user", "super-user"];
+const err = {
+    code: 200,
+    data: new Error(),
+};
+class User {
+}
+class UserPersistence extends User {
+}
+function getUser(dbIdOrId) {
+    if (typeof dbIdOrId === "number") {
+        return new User();
+    }
+    else {
+        return new UserPersistence();
+    }
+}
+function getUser2(id) {
+    if (typeof id === "number") {
+        return new User();
+    }
+    else {
+        return new UserPersistence();
+    }
+}
+const res = getUser2(1);
+const res1 = getUser2("sfdfsffds");
--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,51 @@
-interface Role {
-  name: string;
+const a1: number = Math.random() > 0.5 ? 1 : 0;
+
+interface HTTPResponse<T extends "success" | "failed"> {
+  code: number;
+  data: T extends "success" ? string : Error;
+  // additionalData: T extends "success" ? string : number;
 }
 
-interface Permission {
-  endDate: Date;
-}
-interface User {
-  name: string;
-  roles: Role[];
-  permission: Permission;
-}
-
-const user: User = {
-  name: "Vasya",
-  roles: [],
-  permission: {
-    endDate: new Date(),
-  },
+const suc: HTTPResponse<"success"> = {
+  code: 200,
+  data: "done",
 };
 
-const nameUser = user["name"];
-const roleNames = "roles";
-type rolesType2 = User[typeof roleNames];
-// type rolesType = User["roles"];
+const err: HTTPResponse<"failed"> = {
+  code: 200,
+  data: new Error(),
+};
 
-type roleType = User["roles"][number];
-type dateType = User["permission"]["endDate"];
+class User {
+  id: number;
+  name: string;
+}
 
-const roles = ["admin", "user", "super-user"] as const;
+class UserPersistence extends User {
+  dbId: string;
+}
 
-type roleTypes = typeof roles[number];
+function getUser(id: number): User;
+function getUser(dbId: string): UserPersistence;
+function getUser(dbIdOrId: string | number): User | UserPersistence {
+  if (typeof dbIdOrId === "number") {
+    return new User();
+  } else {
+    return new UserPersistence();
+  }
+}
+
+type UserOrUserPersistence<T extends string | number> = T extends number
+  ? User
+  : UserPersistence;
+
+function getUser2<T extends string | number>(id: T): UserOrUserPersistence<T> {
+  if (typeof id === "number") {
+    return new User() as UserOrUserPersistence<T>;
+  } else {
+    return new UserPersistence() as UserOrUserPersistence<T>;
+  }
+}
+
+const res = getUser2(1);
+const res1 = getUser2("sfdfsffds");
