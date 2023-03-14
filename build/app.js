199e68deb00570621c47cf44b1aba0d42f194d41
verter187 <verter187@inbox.ru>
2023-03-14 19:34:47 +0300

    Infer

--- build/app.js
+++ build/app.js
@@ -1,32 +1,9 @@
 "use strict";
-const a1 = Math.random() > 0.5 ? 1 : 0;
-const suc = {
-    code: 200,
-    data: "done",
+function runTransaction(transaction) {
+    console.log(transaction);
+}
+const transaction = {
+    // fromTo: ["1", "2"] as [string, string], // Хардкорный вариант
+    fromTo: ["1", "2"],
 };
-const err = {
-    code: 200,
-    data: new Error(),
-};
-class User {
-}
-class UserPersistence extends User {
-}
-function getUser(dbIdOrId) {
-    if (typeof dbIdOrId === "number") {
-        return new User();
-    }
-    else {
-        return new UserPersistence();
-    }
-}
-function getUser2(id) {
-    if (typeof id === "number") {
-        return new User();
-    }
-    else {
-        return new UserPersistence();
-    }
-}
-const res = getUser2(1);
-const res1 = getUser2("sfdfsffds");
+runTransaction(transaction);
--- src/app.ts
+++ src/app.ts
@@ -1,51 +1,14 @@
-const a1: number = Math.random() > 0.5 ? 1 : 0;
-
-interface HTTPResponse<T extends "success" | "failed"> {
-  code: number;
-  data: T extends "success" ? string : Error;
-  // additionalData: T extends "success" ? string : number;
+function runTransaction(transaction: { fromTo: [string, string] }) {
+  console.log(transaction);
 }
 
-const suc: HTTPResponse<"success"> = {
-  code: 200,
-  data: "done",
+const transaction: GetFirstArg<typeof runTransaction> = {
+  // fromTo: ["1", "2"] as [string, string], // Хардкорный вариант
+  fromTo: ["1", "2"],
 };
 
-const err: HTTPResponse<"failed"> = {
-  code: 200,
-  data: new Error(),
-};
+runTransaction(transaction);
 
-class User {
-  id: number;
-  name: string;
-}
-
-class UserPersistence extends User {
-  dbId: string;
-}
-
-function getUser(id: number): User;
-function getUser(dbId: string): UserPersistence;
-function getUser(dbIdOrId: string | number): User | UserPersistence {
-  if (typeof dbIdOrId === "number") {
-    return new User();
-  } else {
-    return new UserPersistence();
-  }
-}
-
-type UserOrUserPersistence<T extends string | number> = T extends number
-  ? User
-  : UserPersistence;
-
-function getUser2<T extends string | number>(id: T): UserOrUserPersistence<T> {
-  if (typeof id === "number") {
-    return new User() as UserOrUserPersistence<T>;
-  } else {
-    return new UserPersistence() as UserOrUserPersistence<T>;
-  }
-}
-
-const res = getUser2(1);
-const res1 = getUser2("sfdfsffds");
+type GetFirstArg<T> = T extends (first: infer First, ...args: any[]) => any
+  ? First
+  : never;
