ffc6dc74f288dfde4f512d92660b201de93d4c0a
verter187 <verter187@inbox.ru>
2023-03-16 20:30:53 +0300

    Декоратор класса

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ interface IUserService {
   users: number;
   getUsersInDatabase(): number;
 }
-
+// @nullUser
+@threeUserAdvanced
 class UserService implements IUserService {
   users: number = 1000;
 
@@ -11,15 +12,16 @@ class UserService implements IUserService {
   }
 }
 
-function nullUser(obj: IUserService): IUserService {
-  obj.users = 0;
-  return obj;
+function nullUser(target: Function) {
+  target.prototype.users = 0;
 }
 
-function logUsers(obj: IUserService): IUserService {
-  console.log("Users: ", obj.users);
-  return obj;
+function threeUserAdvanced<T extends { new (...args: any[]): {} }>(
+  constructor: T
+) {
+  return class extends constructor {
+    users = 3;
+  };
 }
 
 console.log(new UserService().getUsersInDatabase());
-console.log(logUsers(nullUser(new UserService())).getUsersInDatabase());
