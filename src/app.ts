df9811de83358efc160e6371c459dd584903f0a7
verter187 <verter187@inbox.ru>
2023-03-19 15:26:59 +0300

    Упражнение - Декоратор перехвата ошибок

--- src/app.ts
+++ src/app.ts
@@ -5,23 +5,30 @@ interface IUserService {
 class UserService implements IUserService {
   users: number = 1000;
 
-  @Log
+  @Catch({ rethrow: false })
   getUsersInDatabase(): number {
-    throw new Error("Error!");
+    throw new Error("Ошибка!");
   }
 }
-function Log(
-  target: Object,
-  propertyKey: string | symbol,
-  descriptor: TypedPropertyDescriptor<(...args: any[]) => any>
-): TypedPropertyDescriptor<(...args: any[]) => any> | void {
-  console.log(target);
-  console.log(propertyKey);
-  console.log(descriptor);
-
-  descriptor.value = () => {
-    console.log("no error");
+function Catch({ rethrow }: { rethrow: boolean } = { rethrow: true }) {
+  return (
+    target: Object,
+    _: string | symbol,
+    descriptor: TypedPropertyDescriptor<(...args: any[]) => any>
+  ): TypedPropertyDescriptor<(...args: any[]) => any> | void => {
+    const method = descriptor.value;
+    descriptor.value = async (...args: any[]) => {
+      try {
+        return await method?.apply(target, args);
+      } catch (e) {
+        if (e instanceof Error) {
+          console.log(e.message);
+          if (rethrow) {
+            throw e;
+          }
+        }
+      }
+    };
   };
 }
-
 console.log(new UserService().getUsersInDatabase());
